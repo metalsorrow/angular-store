6f3fa492bd621b326cb1ae999bfc1c0341089786
marcos.fuentes <mfuentes@sixbell.com>
2021-08-02 09:13:57 -0400

    base template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
+
 })
 export class AppComponent {
+  items: Array<string>;
+  products: Array<Product>;
   title = 'angular-base';
+
+  constructor() {
+    this.items = [];
+    this.products = [{
+      id: 1,
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla'
+
+    }]
+  }
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1)
+  }
+
+  clickProudct(id: number) {
+    console.log(`product: ${id}`);
+  }
 }
---
+++ src/app/components/product.components.ts
@@ -0,0 +1,61 @@
+import { 
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    // DoCheck,
+    OnInit, 
+    SimpleChanges, 
+    OnDestroy, 
+    OnChanges
+} from "@angular/core";
+
+import { Product } from "src/models/product.model";
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+})
+// export class ProductComponent implements OnChanges, OnInit {
+export class ProductComponent implements OnInit, OnChanges, OnDestroy {
+
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor(){
+        this.product = {};
+        console.log('constructor');
+    }
+
+    ngOnChanges(changes: SimpleChanges){
+        // Deteccion Automatica de cambios
+        console.log('ngOnChange');
+        console.log(changes);
+    }
+
+    ngOnInit(){
+        console.log('ngOnInit');
+    }
+
+    // ngDoCheck(){
+    //     // Detecciona manual de cambios
+    //     console.log('doCheck');
+    // }
+
+    ngOnDestroy(){
+        console.log('onDestroy');
+    }
+
+    addCart(){
+        console.log('añadir al carrito');
+        this.productClicked.emit(this.product.id)
+    }
+    // product = {
+    //     id: '1',
+    //     image: 'assets/images/camiseta.png',
+    //     title: 'Camiseta',
+    //     price: 80000,
+    //     description: 'bla bla bla bla'
+    // }
+}
